9cc9b78d98252970cc7d5c49687166ed860e40fe
shitagon@walla.com <danik270694>
2020-03-29 20:15:14 +0300

    Updated MP3Player, Now looking like a product

--- app/src/main/java/com/example/mp3player/MainActivity.java
+++ app/src/main/java/com/example/mp3player/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -18,12 +20,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -39,32 +45,67 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar; //ref to seek bar in ui
     Runnable runnable; //ref to new thread that are going to asynchronously run.
     Handler handler; //get os refresh rate , window utilization and all real-timeness.
-    ArrayList<Integer> playlist=new ArrayList<>(); //will be used to host playlist
-    Thread playThrd,playSeekThrd,stopThrd; //play threads to be used explicitly
+    ArrayList<Integer> playlist = new ArrayList<>(); //will be used to host playlist
+    Thread playThrd, playSeekThrd, stopThrd; //play threads to be used explicitly
     Spinner spinner; //to show the songs
-    public int CurrentSong=R.raw.song,CurrentSongListIndex=0;
+    Switch urlRawSwitch; //to choose RAW Storage or URL play show
+    TextView urlText;
+    Button urlStramBtn;
+    MediaPlayer mediaPlayer2;
+    public int CurrentSong = R.raw.song, CurrentSongListIndex = 0;
+    public boolean isSDSource = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       mediaPlayer=MediaPlayer.create(getApplicationContext(),CurrentSong);
-       seekBar=(SeekBar)findViewById(R.id.seekBar);
-        handler=new Handler();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), CurrentSong);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        handler = new Handler();
         AboutMe(null);
-        spinner=findViewById(R.id.spinner);
-        spinner.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,fillPlaylistFromRes()));
+        spinner = findViewById(R.id.spinner);
+        urlRawSwitch = findViewById(R.id.switch1);
+        urlText = findViewById(R.id.urlText);
+        urlStramBtn = findViewById(R.id.streamBtn);
+
+        ///////////////////////SWITCH///////////////////////////////////////
+        urlRawSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (urlRawSwitch.isChecked()) {
+                    urlText.setVisibility(View.VISIBLE);
+                    urlStramBtn.setVisibility(View.VISIBLE);
+                    spinner.setVisibility(View.INVISIBLE);
+                    mediaPlayer2=new MediaPlayer();
+                    Toast.makeText(getApplicationContext(), "URL Mode , Input MP3 DirectLink", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    urlText.setVisibility(View.INVISIBLE);
+                    urlStramBtn.setVisibility(View.INVISIBLE);
+                    spinner.setVisibility(View.VISIBLE);
+                    mediaPlayer2.release();
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), CurrentSong);
+                    Toast.makeText(getApplicationContext(), "Normal RAW Storage Mode", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        ///////////////////////////////////////////////SPINNER////////////////////////////////////////
+        spinner.setAdapter(new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, fillPlaylistFromRes()));
 
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectSongSpinner=parent.getItemAtPosition(position).toString();
-                CurrentSong=giveNameGetID(selectSongSpinner);
+                String selectSongSpinner = parent.getItemAtPosition(position).toString();
+                CurrentSong = giveNameGetID(selectSongSpinner);
                 mediaPlayer.reset();
-                mediaPlayer=MediaPlayer.create(getApplicationContext(),CurrentSong);
-                handler.postDelayed(stopThrd,1000);
-                handler.postDelayed(playThrd,1000);
-                handler.postDelayed(playSeekThrd,1000);
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), CurrentSong);
+                handler.postDelayed(stopThrd, 1000);
+                handler.postDelayed(playThrd, 1000);
+                handler.postDelayed(playSeekThrd, 1000);
+
             }
 
             @Override
@@ -73,63 +114,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+ /////////////////////////SEEK BAR ///////////////////////////////////////
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-       seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-           @Override
-           public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-               if(fromUser) mediaPlayer.seekTo(progress);
-           }
+                if (fromUser && !urlRawSwitch.isChecked()) mediaPlayer.seekTo(progress);
+                else if(fromUser && urlRawSwitch.isChecked()) mediaPlayer2.seekTo(progress);
+            }
 
-           @Override
-           public void onStartTrackingTouch(SeekBar seekBar) {
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-           }
+            }
 
-           @Override
-           public void onStopTrackingTouch(SeekBar seekBar) {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
-           }
-       });
+            }
+        });
         PlayingEvents();
         fillPlaylistFromRes();
     }
 
     @SuppressLint("ResourceType")
-    public int giveNameGetID(String songName)
-    {
-        return getResources().getIdentifier(songName,"raw",this.getPackageName());
+    public int giveNameGetID(String songName) {
+        return getResources().getIdentifier(songName, "raw", this.getPackageName());
     }
 
 
     public ArrayList<String> fillPlaylistFromRes() {
-        Field[] inRawArr=R.raw.class.getFields();
-        ArrayList<String> namesForSpinner=new ArrayList<String>();
-        for(int i=0;i<inRawArr.length;i++)
-        {
+        Field[] inRawArr = R.raw.class.getFields();
+        ArrayList<String> namesForSpinner = new ArrayList<String>();
+        for (int i = 0; i < inRawArr.length; i++) {
             namesForSpinner.add(inRawArr[i].getName());
-            playlist.add(getResources().getIdentifier(inRawArr[i].getName(),"raw",this.getPackageName()));
-            Log.d("Filling the Field[]", "fillPlaylistFromRes: "+inRawArr[i].getName()+" "+playlist.get(i));
+            playlist.add(getResources().getIdentifier(inRawArr[i].getName(), "raw", this.getPackageName()));
+            Log.d("Filling the Field[]", "fillPlaylistFromRes: " + inRawArr[i].getName() + " " + playlist.get(i));
 
         }
         return namesForSpinner;
     }
 
-    public CharSequence nicelyFormat(int timeMs){
-        String TAG="Checking";
-        CharSequence cs="";
-        Log.d(TAG, "nicelyFormat: "+timeMs/1000);
-        int timeInSec=timeMs/1000;
-        int Min=timeInSec/60;
-        int Sec=timeInSec-Min*60;
+    public CharSequence nicelyFormat(int timeMs) {
+        String TAG = "Checking";
+        CharSequence cs = "";
+        Log.d(TAG, "nicelyFormat: " + timeMs / 1000);
+        int timeInSec = timeMs / 1000;
+        int Min = timeInSec / 60;
+        int Sec = timeInSec - Min * 60;
 
-        return ""+String.valueOf(Min)+":"+ (Sec<10? "0"+String.valueOf(Sec):String.valueOf(Sec));
+        return "" + String.valueOf(Min) + ":" + (Sec < 10 ? "0" + String.valueOf(Sec) : String.valueOf(Sec));
     }
 
-    public void PlayingEvents(){
-        String TAG="Debug";
+    public void PlayingEvents() {
+        String TAG = "Debug";
         Log.d(TAG, "PlayingEvents: HERE ");
-        if(mediaPlayer.isPlaying()){
-            runnable= new Runnable() {
+        if (!urlRawSwitch.isChecked()) { //////////////////////if normal raw player.. then..
+
+
+        if (mediaPlayer.isPlaying()) {
+            runnable = new Runnable() {
                 @Override
                 public void run() {
                     timeText.setText(nicelyFormat(mediaPlayer.getCurrentPosition())); //set time val
@@ -138,10 +182,8 @@ public class MainActivity extends AppCompatActivity {
 
             };
 
-        }
-        else
-        {
-            runnable= new Runnable() {
+        } else {
+            runnable = new Runnable() {
                 @Override
                 public void run() {
 
@@ -150,8 +192,38 @@ public class MainActivity extends AppCompatActivity {
 
             };
         }
-        handler.postDelayed(runnable,1000);
+        handler.postDelayed(runnable, 1000);
     }
+        else //////////////////////////////URL Player
+        {
+            if (mediaPlayer2.isPlaying()) {
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        timeText=findViewById(R.id.songName2);
+                        timeText.setText(nicelyFormat(mediaPlayer2.getCurrentPosition())); //set time val
+                        PlayingEvents();
+                    }
+
+                };
+
+            } else {
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        PlayingEvents();
+                    }
+
+                };
+            }
+            handler.postDelayed(runnable, 1000);
+        }
+    }
+
+
+
+
 
 
     public void mediaIconSet(int sel){ //i decided that 0-stop 1-play 2-pause
@@ -262,5 +334,91 @@ public class MainActivity extends AppCompatActivity {
 
     public void AboutMe(View view) {
         Toast.makeText(getApplicationContext(),"AudioPlayer was developed by Daniel Ohayon for Matan Nir",Toast.LENGTH_LONG).show();
+    }
+
+    public void Prev(View view) {
+        if(CurrentSongListIndex==0)
+        {
+            CurrentSong=playlist.get(CurrentSongListIndex);
+            CurrentSongListIndex=playlist.size()-1;
+        }
+        else {
+            CurrentSong = playlist.get(CurrentSongListIndex--);
+        }
+
+        mediaPlayer.reset();
+        playBtn.setText("Play");
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),CurrentSong);
+        handler.postDelayed(stopThrd,1000);
+        handler.postDelayed(playThrd,1000);
+        handler.postDelayed(playSeekThrd,1000);
+    }
+
+    public void prepareURLStream(String url)
+    {
+
+        try {
+            mediaPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer2.setDataSource(url);
+            Toast.makeText(getApplicationContext(),"Buffering Media..",Toast.LENGTH_SHORT).show();
+            mediaPlayer2.prepare();
+        } catch (IOException e) {
+            Log.e("STREAM-ERR", "run: ", e);
+            Toast.makeText(getApplicationContext(),"Failed to Load Stream From URL, Check The Link",Toast.LENGTH_LONG).show();
+        }
+    }
+    public void StreamUrl(View view) {
+        final Button streamBtn=findViewById(R.id.streamBtn);
+            new Thread(){
+                public void run(){
+                    final CharSequence streamURI=urlText.getText();
+                    prepareURLStream(streamURI.toString());
+                    songName=findViewById(R.id.songName);
+
+                    PlayingEvents();
+
+
+                    seekBar.setMax(mediaPlayer2.getDuration());
+
+                    final GifImageView eqAnim=(GifImageView)findViewById(R.id.eqAnim);
+                    playThrd=new Thread(){
+                        @SuppressLint("ResourceType")
+                        public void run(){
+
+                            if(streamBtn.getText().equals("Stream")) {
+                                mediaPlayer2.start();
+                                mediaIconSet(1);
+                                eqAnim.setVisibility(View.VISIBLE);
+                                //songName.setText(streamURI);
+                            }
+                            else {
+                                mediaPlayer2.pause();
+                                mediaIconSet(2);
+                                eqAnim.setVisibility(View.INVISIBLE);
+                            }
+                            if(mediaPlayer2.isPlaying()) {
+                                streamBtn.setText("Pause");
+                            }
+                            else
+                                streamBtn.setText("Stream");
+                        }
+                    };
+                    playThrd.run();
+
+                    playSeekThrd=new Thread(){
+
+                        public void run()
+                        {
+
+                            Log.d("Seek Debug", "run: ");
+                            seekBar.setProgress(mediaPlayer2.getCurrentPosition()); //set seek val
+                            handler.postDelayed(this,1000);
+                        }
+
+                    };
+                    playSeekThrd.start();
+                }
+        }.run();
+
     }
 }
